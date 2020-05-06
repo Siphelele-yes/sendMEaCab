dfeb2267b5a144602f48cc6aa7473520bb4851b9
Siphelele-yes <56165006+Siphelele-yes@users.noreply.github.com>
2020-05-06 06:56:25 +0200

    Progress bar for background operation

--- app/src/main/java/za/co/sendmedelivery/sendmeacab/MainActivity.java
+++ app/src/main/java/za/co/sendmedelivery/sendmeacab/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -52,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
     class BackgroundTask extends AsyncTask<String, Void, String> {
 
         String dbURL;
+        private ProgressBar progressBar;
         @Override
         protected void onPreExecute() {
 
@@ -60,9 +62,9 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... args) {
-            String email=emailText,password=passwordText;
-            email= args[0];
-            password=args[1];
+
+            emailText= args[0];
+            passwordText=args[1];
 
             try {
                 URL url = new URL(dbURL);
@@ -74,8 +76,8 @@ public class MainActivity extends AppCompatActivity {
                     OutputStream outputStream = httpURLConnection.getOutputStream();
                     BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
 
-                    String details = URLEncoder.encode("email","UTF-8")+ "="+URLEncoder.encode(email,"UTF-8")+"&"
-                            +URLEncoder.encode("password","UTF-8")+ "="+URLEncoder.encode(password,"UTF-8");
+                    String details = URLEncoder.encode("email","UTF-8")+ "="+URLEncoder.encode(emailText,"UTF-8")+"&"
+                            +URLEncoder.encode("password","UTF-8")+ "="+URLEncoder.encode(passwordText,"UTF-8");
 
                     bufferedWriter.write(details);
                     bufferedWriter.flush();
@@ -83,9 +85,9 @@ public class MainActivity extends AppCompatActivity {
                     outputStream.close();
 
                     InputStream inputStream = httpURLConnection.getInputStream();
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.ISO_8859_1));
                     String response = "";
-                    String line = "";
+                    String line;
 
                     while ((line = bufferedReader.readLine())!=null) response += line;
 
@@ -115,7 +117,8 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String aVoid) {
-            super.onPostExecute(aVoid);
+            progressBar = new ProgressBar(MainActivity.this, null, R.layout.loading );
+            progressBar.setVisibility(View.VISIBLE);
         }
     }
 }
