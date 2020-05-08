698a0916029ed762f0b58470b952a2488813b31a
Siphelele-yes <56165006+Siphelele-yes@users.noreply.github.com>
2020-05-08 18:57:36 +0200

    log in Backend

--- app/src/main/java/za/co/sendmedelivery/sendmeacab/MainActivity.java
+++ app/src/main/java/za/co/sendmedelivery/sendmeacab/MainActivity.java
@@ -1,126 +1,64 @@
 package za.co.sendmedelivery.sendmeacab;
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
+import android.widget.Toast;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText Email,Password;
-    String emailText,passwordText;
+    private EditText UsernameET,PasswordET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Email = findViewById(R.id.etEmail);
-        Password = findViewById(R.id.etPassword);
-
+        // Get Reference to variables
+        UsernameET = findViewById(R.id.etEmail);
+        PasswordET = findViewById(R.id.etPassword);
     }
-
+    // Triggers when LOGIN Button clicked
     public void btnSubmit_OnClick(View v){
 
-        emailText = Email.getText().toString();
-        passwordText = Password.getText().toString();
 
-        BackgroundTask backgroundTask = new BackgroundTask();
-        backgroundTask.execute(emailText,passwordText);
+        // Get text from email and password field
+        final String username = UsernameET.getText().toString();
+        final String password = PasswordET.getText().toString();
 
-        startActivity((new Intent(MainActivity.this, DefineTripActivity.class)));
-    }
-
-    class BackgroundTask extends AsyncTask<String, Void, String> {
-
-        String dbURL;
-        private ProgressBar progressBar;
-        @Override
-        protected void onPreExecute() {
-
-            //dbURL=//get URL for db (domain name + /php loginfile name);
-        }
-
-        @Override
-        protected String doInBackground(String... args) {
-
-            emailText= args[0];
-            passwordText=args[1];
-
-            try {
-                URL url = new URL(dbURL);
-                try {
-                    HttpURLConnection httpURLConnection =(HttpURLConnection) url.openConnection();
-                    httpURLConnection.setRequestMethod("POST");
-                    httpURLConnection.setDoOutput(true);
-                    httpURLConnection.setDoInput(true);
-                    OutputStream outputStream = httpURLConnection.getOutputStream();
-                    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
-
-                    String details = URLEncoder.encode("email","UTF-8")+ "="+URLEncoder.encode(emailText,"UTF-8")+"&"
-                            +URLEncoder.encode("password","UTF-8")+ "="+URLEncoder.encode(passwordText,"UTF-8");
-
-                    bufferedWriter.write(details);
-                    bufferedWriter.flush();
-                    bufferedWriter.close();
-                    outputStream.close();
-
-                    InputStream inputStream = httpURLConnection.getInputStream();
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.ISO_8859_1));
-                    String response = "";
-                    String line;
-
-                    while ((line = bufferedReader.readLine())!=null) {
-                        response += line;
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://sendmedelivery.co.za/aCab/App/login.php",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getApplicationContext(), "This is a response: " + response, Toast.LENGTH_LONG).show();
                     }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
 
-                    bufferedReader.close();
-                    inputStream.close();
-                    httpURLConnection.disconnect();
-
-                    return response;
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
             }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("username",username);
+                params.put("password",password);
+                return params;
+            }
+        };
+        Volley.newRequestQueue(this).add(stringRequest);
 
-            return null;
-        }
-
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-        }
-
-
-
-        @Override
-        protected void onPostExecute(String aVoid) {
-            progressBar = findViewById(R.id.progressbar);
-            progressBar.setVisibility(View.VISIBLE);
-        }
     }
+
+
 }
