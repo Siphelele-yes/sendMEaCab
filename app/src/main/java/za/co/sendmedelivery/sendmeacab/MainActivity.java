11bb7ffdac4f82760d396b5e433f32c5713ff990
SonwabileSigenu <40338577+SonwabileSigenu@users.noreply.github.com>
2020-05-09 00:20:49 +0200

    Revert "Merge pull request #1 from SonwabileSigenu/sendMEDeveloper"
    This reverts commit b95d80860f7bb47b146ff8f4298410eec75fa833, reversing
    changes made to b9b5de1e6b86639c83de8ac5abad5a9e5fd93b3a.
    
    # Conflicts:
    #	app/src/main/java/za/co/sendmedelivery/sendmeacab/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/za/co/sendmedelivery/sendmeacab/MainActivity.java
+++ app/src/main/java/za/co/sendmedelivery/sendmeacab/MainActivity.java
@@ -1,6 +1,17 @@
 package za.co.sendmedelivery.sendmeacab;
 
-
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,8 +38,28 @@ public class MainActivity extends AppCompatActivity {
         // Get Reference to variables
         UsernameET = findViewById(R.id.etEmail);
         PasswordET = findViewById(R.id.etPassword);
+
+        TextView tv_fgt_password = findViewById(R.id.tv_fgt_password);
+        String string_fgt_password = "I forgot my password";
+        SpannableString ss_fgt_password = new SpannableString(string_fgt_password);
+        ClickableSpan clickableSpan_fgt_password = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                startActivity((new Intent(MainActivity.this, ForgotPassword.class)));
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.BLUE);
+                ds.setUnderlineText(false);
+            }
+        };
+        ss_fgt_password.setSpan(clickableSpan_fgt_password, 0, 20, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tv_fgt_password.setText(ss_fgt_password);
+        tv_fgt_password.setMovementMethod(LinkMovementMethod.getInstance());
     }
-    // Triggers when LOGIN Button clicked
+
     public void btnLogin_OnClick(View v){
 
 
@@ -58,6 +89,11 @@ public class MainActivity extends AppCompatActivity {
         };
         Volley.newRequestQueue(this).add(stringRequest);
 
+    }
+
+    public void btnSignUp_OnClick(View v){
+
+        startActivity((new Intent(MainActivity.this, SignUp.class)));
     }
 
 
