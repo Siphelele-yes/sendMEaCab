6ac834a685c0dc2624a69c890250b3b3d6e7d629
SonwabileSigenu <40338577+SonwabileSigenu@users.noreply.github.com>
2020-05-13 03:14:44 +0200

    F/end + layout functionality updates:   DefineTrip Activity removed, replaced with TripDetails Activity   Animations begun but incomplete on app
    App set to portrait mode by default

--- app/src/main/java/za/co/sendmedelivery/sendmeacab/SignUp.java
+++ app/src/main/java/za/co/sendmedelivery/sendmeacab/SignUp.java
@@ -2,10 +2,7 @@ package za.co.sendmedelivery.sendmeacab;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
 
@@ -21,7 +18,7 @@ public class SignUp extends AppCompatActivity {
         //Final app edit ->
         Toast.makeText(getApplicationContext(),"Welcome to sendME. Happy riding!",
                 Toast.LENGTH_LONG);
-        startActivity((new Intent(SignUp.this, DefineTripActivity.class)));
+        startActivity((new Intent(SignUp.this, TripDetails.class)));
         //Final app edit <-
     }
 
